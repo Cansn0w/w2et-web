051f9846edce638341d6d01456b4d7fd108c164e
Chester <cliu4891@uni.sydney.edu.au>
2016-09-29 15:08:51 +1000

    Created basic navigation logics with angular router, created homepage, recipe, and restaurant component with html templates. Created routing to these componenets.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
+import { RecipeComponent } from './recipe/recipe.component';
+
+import { W2etRoutingModule } from './app-routing.module'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RestaurantComponent,
+    RecipeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    W2etRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
