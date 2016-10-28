498fe2b9a4c4ee8577c00e5fb62b5b737f4110a9
namoshizun <dilu3100@gmail.com>
2016-10-28 14:28:31 +1100

    add config

---
+++ src/app/com/config.ts
@@ -0,0 +1,20 @@
+/*
+ * Application-wide config variables can be kept and imported from here
+ */
+
+// RXJS Operators
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/toPromise';
+
+// export const HOST = 'http://192.168.0.200:8000';
+// export const HOST = 'http://127.0.0.1:8000';
+export const HOST = 'https://demo.what2eattoday.com/api';
+
+export const GOOGLE_MAP_API_KEY = 'AIzaSyBynD4V-DFroy61rWE8GeKTfDC34rznRqI';
+export const FACEBOOK_API_KEY = '1862342107330718';
+export const FACEBOOK_APP_NAME = 'What2EatToday';
